07d5385b865c5ceddb114820159e01ba422450da
Karleus <112374531+Karleus@users.noreply.github.com>
2023-08-24 09:07:45 +0300

    Tehtävä tehty

---
+++ tehtävä02c.py
@@ -0,0 +1,7 @@
+kanta = float(input("Syötä suorakulmion kannan pituus: "))
+korkeus = float(input("Syötä suorakulmion korkeus: "))
+
+piiri = kanta*2+korkeus*2
+pintaala = kanta*korkeus
+
+print("Suorakulmion piiri on: " + str(piiri) + " ja pinta ala on: " + str(pintaala))

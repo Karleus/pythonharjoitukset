b06b7e9d2ce07236f107adbc5ca068fa033a211f
Karleus <112374531+Karleus@users.noreply.github.com>
2023-08-24 12:59:18 +0300

    Tehtävä tehty

---
+++ tehtävä02e.py
@@ -0,0 +1,13 @@
+leiviska = float(input("Anna massa leivisköinä: "))
+naula = float(input("Anna massa nauloina: "))
+luoti = float(input("Anna massa luoteina: "))
+
+leiviskakg = leiviska*8512
+naulakg = naula*425.6
+luotikg = luoti*13.3
+
+kilot = (leiviskakg + naulakg + luotikg)/1000
+print (f"tää {kilot})")
+
+
+#hae kg math.floor, modulo eli %
---
+++ tehtävä02f.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.sample(range(0, 9), 3))
+
+print(random.sample(range(1, 6), 4))

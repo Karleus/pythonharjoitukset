367b9147d71d4c985d891bffb9abb508b73cce36
Karleus <112374531+Karleus@users.noreply.github.com>
2023-08-24 11:10:29 +0300

    Tehtävä tehty

---
+++ tehtävä02d.py
@@ -0,0 +1,9 @@
+numero1 = int(input("syötä luku: "))
+numero2 = int(input("syötä toinen luku: "))
+numero3 = int(input("syötä kolmas luku: "))
+
+summa = numero1 + numero2 + numero3
+tulo = numero1 * numero2 * numero3
+keskiarvo = (numero1 + numero2 + numero2)/3
+
+print(f"Lukujen summa on {summa}\ntulo on {tulo}\nkeskiarvo on {keskiarvo}")

b196445c94ac2b9cd29293b00b8a6d7051e01404
Karleus <112374531+Karleus@users.noreply.github.com>
2023-08-29 11:20:38 +0300

    Tehtävä tehty

--- Moduuli3/tehtävä03c.py
+++ Moduuli3/tehtävä03c.py
@@ -1,1 +1,29 @@
-sukupuoli =(input("Anna sukupuolesi"))
+sukupuoli = str(input("Anna sukupuolesi m/n: "))
+
+
+
+if sukupuoli=="m" :
+
+    hba = float(input("syötä hemoglobaani arvosi: "))
+
+    if  hba < 134:
+        print(" Hemoglobaanisi on alhainen")
+
+    elif hba > 195:
+        print("Hemogloobanisi on korkea")
+
+    else: print("Hemoglobaanisi on normaali")
+
+
+if sukupuoli=="n" :
+
+    hba = float(input("syötä hemoglobaani arvosi: "))
+
+    if  hba < 117:
+        print(" Hemoglobaanisi on alhainen")
+
+    elif hba > 175:
+        print("Hemogloobanisi on korkea")
+
+    else: print("Hemoglobaanisi on normaali")
+
